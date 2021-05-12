879274b20245ff991e1aeb1fb2e688897f8af1f9
JoshMas <49138669+JoshMas@users.noreply.github.com>
2021-05-12 17:58:18 +1000

    Updated FPSNetManager

--- Assets/FPS/Scripts/Networking/FPSNetManager.cs
+++ Assets/FPS/Scripts/Networking/FPSNetManager.cs
@@ -5,5 +5,16 @@ using Mirror;
 
 public class FPSNetManager : NetworkManager
 {
-    
+
+    public static FPSNetManager Instance => singleton as FPSNetManager;
+
+    public override void OnServerAddPlayer(NetworkConnection conn)
+    {
+        base.OnServerAddPlayer(conn);
+    }
+
+    public override void OnServerDisconnect(NetworkConnection conn)
+    {
+        base.OnServerDisconnect(conn);
+    }
 }
