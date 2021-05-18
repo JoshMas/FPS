e36393708ebce4739f8f61d4e565868af6060448
Dreadbread76 <61036310+Dreadbread76@users.noreply.github.com>
2021-05-18 23:16:10 +1000

    shooting is good and bloomy

--- Assets/FPS/Scripts/Player/Weaponry/PlayerWeapons.cs
+++ Assets/FPS/Scripts/Player/Weaponry/PlayerWeapons.cs
@@ -2,17 +2,27 @@ using System;
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using Random = UnityEngine.Random;
+
 
 public class PlayerWeapons : MonoBehaviour
 {
-    [SerializeField]
-    public float rateOfPrimaryFire;
+    [SerializeField] public float rateOfPrimaryFire;
 
     private float rechamberTime;
-    [SerializeField]
-    private GameObject secondaryFire;
-    [SerializeField]
-    private float secondaryCooldown;
+    [SerializeField] private GameObject secondaryFire;
+    [SerializeField] private float secondaryCooldown;
+    
+    [SerializeField] private float bloomInc;
+    [SerializeField] private float bloomMin;
+    [SerializeField] private float bloomMax;
+    [SerializeField] private float bloomReduct;
+
+    [SerializeField] private float range;
+    [SerializeField] private int damage;
+    
+    
+    private float bloom;
 
     private Transform camTransform;
     
@@ -27,6 +37,8 @@ public class PlayerWeapons : MonoBehaviour
     {
         camTransform = Camera.main.transform;
         rechamberTime = 0;
+        bloom = bloomMin;
+        
     }
 
     // Update is called once per frame
@@ -35,27 +47,55 @@ public class PlayerWeapons : MonoBehaviour
         if(rechamberTime > 0)
         {
             rechamberTime -= Time.deltaTime;
+            
         }
-        if(rechamberTime == 0)
+        else if(rechamberTime <= 0)
         {
             
-            if (Input.GetMouseButtonDown(0))
+            if (Input.GetMouseButton(0))
             {
                 PrimaryFire();
                
             }
         }
 
-        
+      
+        if(bloom >= bloomMax)
+        {
+            bloom = bloomMax;
+        }
+
+        if (bloom > bloomMin)
+        {
+            bloom -= bloomReduct *  60 * Time.deltaTime;
+        }
+       
             
     }
 
     public void PrimaryFire()
     {
+        Vector3 currentBloom = new Vector3(Random.Range(-bloom, bloom), Random.Range(-bloom, bloom), 0);
+        
+        int layerMask = 1 << 8;
+        layerMask  = ~layerMask;
+        
+        if (bloom < bloomMax)
+        {
+            bloom += bloomInc * 60*  Time.deltaTime;
+            Debug.Log((bloom));
+        }
        
-        Ray bullet = new Ray(camTransform.position, camTransform.TransformPoint(Vector3.forward) - camTransform.position);
+        RaycastHit hit;
+        if (Physics.Raycast(camTransform.position, camTransform.TransformDirection(Vector3.forward)+ gameObject.transform.TransformDirection(Vector3.forward), out hit, 20f, 3 ))
+        {
+            
+        }
+        Debug.DrawLine(camTransform.position, (camTransform.TransformDirection(Vector3.forward) + gameObject.transform.TransformDirection(Vector3.forward) + currentBloom) * 20f, Color.red, 2);
+       
+        
         rechamberTime = rateOfPrimaryFire;
-        Debug.Log(Physics.Raycast(bullet));
+        Debug.Log(hit);
     }
 
     public void SecondaryFire()
