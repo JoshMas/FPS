4dc1aa07871eff23c6323c24eaf775544030a17b
JoshMas <49138669+JoshMas@users.noreply.github.com>
2021-05-24 23:01:04 +1000

    testing raycasts

--- Assets/FPS/Scripts/Player/PlayerWeaponsOld.cs
+++ Assets/FPS/Scripts/Player/PlayerWeaponsOld.cs
@@ -11,6 +11,8 @@ public class PlayerWeaponsOld : MonoBehaviour
     [SerializeField]
     private float secondaryCooldown;
 
+    private float health = 100.0f;
+
     // Start is called before the first frame update
     void Start()
     {
@@ -22,17 +24,28 @@ public class PlayerWeaponsOld : MonoBehaviour
     {
         if (Input.GetMouseButtonDown(0))
             PrimaryFire();
+        if (Input.GetMouseButtonDown(1))
+            SecondaryFire();
     }
 
     public void PrimaryFire()
     {
         Transform camTransform = Camera.main.transform;
         Ray bullet = new Ray(camTransform.position, camTransform.TransformDirection(Vector3.forward));
-        Debug.Log(Physics.Raycast(bullet)); 
+        RaycastHit hit = new RaycastHit();
+        Debug.Log(Physics.Raycast(bullet, out hit));
+        if(hit.transform.CompareTag("Player"))
+            Debug.Log(hit.transform.GetComponent<PlayerWeaponsOld>().LoseHealth());
+    }
+
+    public float LoseHealth()
+    {
+        health -= 10.0f;
+        return health;
     }
 
     public void SecondaryFire()
     {
-
+        Debug.Log(health);
     }
 }
